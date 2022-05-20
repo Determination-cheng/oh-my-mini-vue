28fbe5b12df8c38d571550800bc6186403ef01f1
Cheng <1003306162@qq.com>
2022-05-20 21:51:39 +0800

    feat: 完成 readonly

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,19 +1,50 @@
 import { track, trigger } from './effect'
 
-export function reactive<T extends Record<string, unknown>>(target: T) {
-  return new Proxy(target, {
-    get(target, key) {
-      const res = Reflect.get(target, key)
-      // 依赖收集
-      track(target, key)
-      return res
-    },
+//* reactive
+const reactiveHandler = {
+  get: createGetter(),
+  set: createSetter(),
+}
 
-    set(target, key, value) {
-      const res = Reflect.set(target, key, value)
-      // 触发依赖
-      trigger(target, key)
-      return res
-    },
-  })
+export function reactive<T extends Record<string, unknown>>(target: T) {
+  return new Proxy(target, reactiveHandler)
+}
+
+//* readonly
+const readonlyHandler = {
+  get: createGetter(true),
+  set: createSetter(true),
+}
+
+export function readonly<T extends Record<string, unknown>>(target: T) {
+  return new Proxy(target, readonlyHandler)
+}
+
+function createGetter(isReadonly = false) {
+  return function get(target: Record<string, unknown>, key: string | symbol) {
+    const res = Reflect.get(target, key)
+
+    // 普通的 reactive 会收集依赖
+    if (!isReadonly) track(target, key)
+
+    return res
+  }
+}
+
+function createSetter(isReadonly = false) {
+  return function set(
+    target: Record<string, unknown>,
+    key: string | symbol,
+    value: any,
+  ) {
+    if (isReadonly) {
+      console.warn(`${key.toString()} cannot be set`)
+      return true
+    }
+
+    const res = Reflect.set(target, key, value)
+    // 触发依赖
+    trigger(target, key)
+    return res
+  }
 }
